06c23c7a1a7b1e4c9f99e5766cd476e57234b551
Nauman Irshad Ali Shah <shahjee3565@gmail.com>
2025-03-29 21:04:15 +0500

    Add files via upload

---
+++ varible_lab3.py
@@ -0,0 +1,27 @@
+x=20
+y=3.4
+z="test match "
+w=["car",2,4.6]
+
+print(x)
+print(y)
+print(x)
+print(w)
+print(w[0])
+print(w[1])
+print(w[2])
+
+def my_funtion():
+    print("hello from a funtion")
+    print("hello from a funtion")
+    print("hello from a funtion")
+
+my_funtion()
+    
+
+number = int(input("enbter number"))
+
+for i in range (1,11):
+    print(f"{number}*{i}={number*i}")
+
+                             
